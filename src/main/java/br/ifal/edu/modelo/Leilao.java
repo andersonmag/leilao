5805f3105be8decc8329a3a5d2d0aed75fc19cbe
Anderson <adnrepper@gmail.com>
2019-08-05 10:05:45 -0200

    Metodo propoe e atributo produto da Classe Produto, adicionado à classe Leilao

--- src/main/java/br/ifal/edu/modelo/Leilao.java
+++ src/main/java/br/ifal/edu/modelo/Leilao.java
@@ -5,13 +5,21 @@ import java.util.ArrayList;
 public class Leilao {
 	
 	private ArrayList<Lance> lances;
-	
+	private Produto produto;
+
 	public Leilao() {
 		
 		this.lances = new ArrayList<Lance>();
 		
 	}
 	
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
 
 	public ArrayList<Lance> getLances() {
 		return lances;
@@ -19,6 +27,12 @@ public class Leilao {
 
 	public void setLances(ArrayList<Lance> lances) {
 		this.lances = lances;
+	}
+	
+	public void propoe(Lance lance) {
+		
+		lances.add(lance);
+			
 	}
 	
 
--- src/main/java/br/ifal/edu/modelo/Usuario.java
+++ src/main/java/br/ifal/edu/modelo/Usuario.java
@@ -1,5 +1,30 @@
 package br.ifal.edu.modelo;
 
 public class Usuario {
+	
+	private String nome;
+	private String email;
+	
+	public Usuario(String nome) {
+		
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 
 }
